7aeae70e0365ba7a8490811e525c46f3030b176a
Meash <m4rtinsch@gmail.com>
2016-05-04 14:06:50 -0400

    Adapt hmax train and classify to klab325 data
    train is derived from example.m

--- hmax/classify.m
+++ hmax/classify.m
@@ -6,14 +6,13 @@ USE_SAVED_PREDICTION=true;
 
 %% Load previous output
 if (nargin < 1)
-    stored_output = './output/exampleActivations.mat';
+    stored_output = './output/activations.mat';
 end
 load(stored_output);
-load('./exampleImages.mat'); % for the image filenames and testing
+load('./../data/data_occlusion_klab325v2_origimages.mat'); % labels
 
 % determine labels
-image_filenames = exampleImages;
-labels = get_labels_from_files(image_filenames);
+labels = [data.truth(1:5,1); data.truth(61:65,1)];
 % prepare input (c2)
 c2_bands_pool = poolC2(c2);
 assert(length(labels) == size(c2_bands_pool,1));
@@ -28,13 +27,19 @@ assert(length(labels) == size(c2_bands_pool,1));
 classifier = fitcecoc(c2_bands_pool,labels);
 
 %% test
+% prepare
+load('../data/KLAB325.mat');
+test_indices = [{1,1:5}; {1,61:65}];
+test_imgs = [img_mat(test_indices{1,1},test_indices{1,2}),...
+    img_mat(test_indices{2,1},test_indices{2,2})];
+real_test_labels = [data.truth(test_indices{1,2},test_indices{1,1});...
+    data.truth(test_indices{2,2},test_indices{2,1})];
 % predict
-test_imgs=exampleImages;
-saved_c2test_file='./output-test/exampleActivations.mat';
+saved_c2test_file='./output-test/activations.mat';
 if USE_SAVED_C2TEST
     load(saved_c2test_file);
 else
-    [c2_test,c1_test]=example(test_imgs,'./output-test/');
+    [c2_test,c1_test]=train(test_imgs,'./output-test/');
 end
 saved_predictions_file='./output-test/predictions.mat';
 if USE_SAVED_PREDICTION
@@ -45,14 +50,15 @@ else
     save(saved_predictions_file, 'c2_test_bands_pool', 'predicted_labels');
 end
 % analyze
-real_test_labels = get_labels_from_files(test_imgs);
 results = cell(length(test_imgs),1);
 results(:) = {'ERR'};
 for i=1:length(test_imgs)
-    if predicted_labels{i} == real_test_labels{i}
+    if predicted_labels(i) == real_test_labels(i)
         results{i} = 'OK';
     end
 end
-zipped_results=[results(:)';predicted_labels(:)';real_test_labels(:)'];
-fprintf('[%s] Prediction: %s | Real: %s\n', zipped_results{:});
+zipped_results=[results(:)';...
+    num2cell(predicted_labels(:)');...
+    num2cell(real_test_labels(:)')];
+fprintf('[%s] Prediction: %d | Real: %d\n', zipped_results{:});
 end
