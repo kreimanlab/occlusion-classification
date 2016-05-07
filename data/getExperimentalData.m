edb7e32de4a65e8a1c2b3c83f9cafea5fc3e45df
Meash <m4rtinsch@gmail.com>
2016-05-07 08:33:05 -0400

    Allow passing selection for data

--- data/getExperimentalData.m
+++ data/getExperimentalData.m
@@ -1,11 +1,13 @@
 function [images, labels] = ...
-    getExperimentalData(dataFile, labelFile)
-if nargin < 2
+    getExperimentalData(selection, dataFile, labelFile)
+if nargin < 1
+    selection = [1:3, 61:63];
     dataFile = 'KLAB325.mat';
     labelFile = 'data_occlusion_klab325v2_origimages';
 end
 
 load(dataFile);
-images = [img_mat(1,1:5), img_mat(1,61:65)];
 load(labelFile);
-labels = [data.truth(1:5,1); data.truth(61:65,1)];
+
+images = img_mat(1,selection);
+labels = data.truth(selection,1);
