59bad15c0d5dc15b7c2615eb08acf3399aff476d
Meash <m4rtinsch@gmail.com>
2016-05-07 23:51:05 -0400

    use MD5 of images as savefile identifier

--- runClassifier.m
+++ runClassifier.m
@@ -2,7 +2,7 @@ function results = runClassifier(classifier, ...
     trainImages, trainLabels, ...
     testImages, testLabels)
 
-if nargin < 1
+if nargin < 5
     error('not enough arguments');
 end
 
@@ -15,7 +15,8 @@ end
 fprintf('Training %s...\n', classifier.getName());
 numTrainImages = length(trainImages);
 % extract features
-trainFeaturesSaveFile = [saveFolder '/train_features-' num2str(numTrainImages) '.mat'];
+trainFeaturesSaveFile = [saveFolder '/train_features-' ...
+    GetMD5(trainImages, 'Array') '.mat'];
 if exist(trainFeaturesSaveFile, 'file')
     fprintf('Loading saved train features %s\n', trainFeaturesSaveFile)
     load(trainFeaturesSaveFile);
@@ -30,7 +31,8 @@ classifier.fit(trainFeatures, trainLabels);
 fprintf('Testing %s...\n', classifier.getName());
 numTestImages = length(testImages);
 % predict
-testFeaturesSaveFile = [saveFolder '/test_features-' num2str(numTestImages) '.mat'];
+testFeaturesSaveFile = [saveFolder '/test_features-' ...
+    GetMD5(testImages, 'Array') '.mat'];
 if exist(testFeaturesSaveFile, 'file')
     fprintf('Loading saved test features %s\n', testFeaturesSaveFile);
     load(testFeaturesSaveFile);
@@ -38,7 +40,8 @@ else
     testFeatures = classifier.extractFeatures(testImages);
     save(testFeaturesSaveFile, 'testFeatures');
 end
-predictionsSaveFile = [saveFolder '/predictions-' num2str(numTestImages) '.mat'];
+predictionsSaveFile = [saveFolder '/predictions-' ...
+    GetMD5(testImages, 'Array') '.mat'];
 if exist(predictionsSaveFile, 'file')
     fprintf('Loading saved predictions %s\n', predictionsSaveFile);
     load(predictionsSaveFile);
@@ -47,7 +50,8 @@ else
     save(predictionsSaveFile, 'predictedLabels');
 end
 % analyze
-resultsSaveFile = [saveFolder '/results-' num2str(numTestImages) '.mat'];
+resultsSaveFile = [saveFolder '/results-' ...
+    GetMD5(testImages, 'Array') '.mat'];
 [matched, accuracy] = analyzeResults(predictedLabels, testLabels);
 results = struct('name', classifier.getName(), ...
     'predicted', predictedLabels, 'real', testLabels,...
