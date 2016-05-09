1bf23b8b38a10ece89574f3487e61f60cc153f6a
Meash <m4rtinsch@gmail.com>
2016-05-08 20:27:58 -0400

    Select all images by default

--- data/getExperimentalData.m
+++ data/getExperimentalData.m
@@ -1,13 +1,14 @@
 function [images, labels] = ...
-    getExperimentalData(selection, dataFile, labelFile)
+    getExperimentalData(selection)
 if nargin < 1
-    selection = [1:3, 61:63];
-    dataFile = 'KLAB325.mat';
-    labelFile = 'data_occlusion_klab325v2_origimages';
+    selection = 1:325;
 end
+dataFile = 'KLAB325.mat';
+labelFile = 'data_occlusion_klab325v2_origimages';
+
 
 load(dataFile);
 load(labelFile);
 
-images = img_mat(1,selection);
-labels = data.truth(selection,1);
+images = img_mat(1, selection);
+labels = data.truth(selection, 1);
