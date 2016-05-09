8b7af3396f20ffb5320483abd4d38fec9625d285
Meash <m4rtinsch@gmail.com>
2016-05-09 10:57:32 -0400

    rename getExperimentalData -> getWholeImages

--- data/getWholeImages.m
+++ data/getWholeImages.m
@@ -1,12 +1,10 @@
 function [images, labels] = ...
-    getExperimentalData(selection)
+    getWholeImages(selection)
 if nargin < 1
     selection = 1:325;
 end
 dataFile = 'KLAB325.mat';
 labelFile = 'data_occlusion_klab325v2_origimages';
-
-
 load(dataFile);
 load(labelFile);
 
