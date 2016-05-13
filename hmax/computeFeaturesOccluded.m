4d59a5722bee49201c0efaa96d3d03f434960419
Meash <m4rtinsch@gmail.com>
2016-05-13 16:17:44 -0400

    compute all hmax features (whole+occluded)

---
+++ hmax/computeFeaturesOccluded.m
@@ -0,0 +1,30 @@
+function computeFeaturesOccluded()
+
+dir = fileparts(mfilename('fullpath'));
+addpath([dir '/../data']);
+
+wholeImages = getWholeImages();
+occlusionData = load([dir '/../data/data_occlusion_klab325v2.mat']);
+occlusionData = occlusionData.data;
+
+features = repmat(struct('c2', [], 's2', [], 'c1', [], 's1', []), ...
+    size(occlusionData, 1), 1);
+featuresIter = 1;
+for dataIter = 1:size(occlusionData, 1)
+    imageNum = occlusionData.pres(dataIter);
+    occludedImage = occlude(wholeImages(imageNum), dataIter, occlusionData);
+    [c2,c1,~,~,s2,s1] = runHmax(occludedImage);
+    features(featuresIter).c2 = c2;
+    features(featuresIter).c1 = c1;
+    features(featuresIter).s2 = s2;
+    features(featuresIter).s1 = s1;
+    
+    if mod(dataIter, 1000) == 0
+        save([dir '/../data/OcclusionModeling/features/data_occlusion_klab325v2/'...
+            'hmax_all_' num2str(dataIter-999) '-' num2str(dataIter) '.mat'], ...
+            'features');
+        featuresIter = 1;
+    else
+        featuresIter = featuresIter + 1;
+    end
+end
---
+++ hmax/computeFeaturesWhole.m
@@ -0,0 +1,17 @@
+function computeFeaturesWhole()
+
+dir = fileparts(mfilename('fullpath'));
+addpath([dir '/../data']);
+
+wholeImages = getWholeImages();
+features = repmat(struct('c2', [], 's2', [], 'c1', [], 's1', []), ...
+    length(wholeImages), 1);
+for i = 1:length(wholeImages)
+    [c2,c1,~,~,s2,s1] = runHmax(wholeImages(i));
+    features(i).c2 = c2;
+    features(i).c1 = c1;
+    features(i).s2 = s2;
+    features(i).s1 = s1;
+end
+save([dir '/../data/OcclusionModeling/features/klab325_orig/hmax_all_1-325.mat'], ...
+    'features');
