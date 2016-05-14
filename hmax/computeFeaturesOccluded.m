28abb2beff620c3b22acaa2d19dda02dd608bc3f
Meash <m4rtinsch@gmail.com>
2016-05-13 21:37:20 -0400

    decrease write-out lengths

--- hmax/computeFeaturesOccluded.m
+++ hmax/computeFeaturesOccluded.m
@@ -3,15 +3,18 @@ function computeFeaturesOccluded()
 dir = fileparts(mfilename('fullpath'));
 addpath([dir '/../data']);
 
+writeOutEvery = 100;
+
 wholeImages = getWholeImages();
 occlusionData = load([dir '/../data/data_occlusion_klab325v2.mat']);
 occlusionData = occlusionData.data;
 
 features = repmat(struct('c2', [], 's2', [], 'c1', [], 's1', []), ...
-    size(occlusionData, 1), 1);
+    writeOutEvery, 1);
+totalLength = size(occlusionData, 1);
 featuresIter = 1;
-for dataIter = 1:size(occlusionData, 1)
-    fprintf('%d/%d\n', dataIter, size(occlusionData, 1));
+for dataIter = 1:totalLength
+    fprintf('%d/%d\n', dataIter, totalLength);
     imageNum = occlusionData.pres(dataIter);
     occludedImage = occlude(wholeImages(imageNum), dataIter, occlusionData);
     [c2,c1,~,~,s2,s1] = runHmax(occludedImage);
@@ -20,9 +23,9 @@ for dataIter = 1:size(occlusionData, 1)
     features(featuresIter).s2 = s2;
     features(featuresIter).s1 = s1;
     
-    if mod(dataIter, 1000) == 0
+    if mod(dataIter, writeOutEvery) == 0 || dataIter == totalLength
         save([dir '/../data/OcclusionModeling/features/data_occlusion_klab325v2/'...
-            'hmax_all_' num2str(dataIter-999) '-' num2str(dataIter) '.mat'], ...
+            'hmax_all_' num2str(dataIter-writeOutEvery+1) '-' num2str(dataIter) '.mat'], ...
             'features');
         featuresIter = 1;
     else
