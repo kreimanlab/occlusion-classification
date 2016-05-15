8082821af5df34b371ca73233ccd93c15dafdbe3
Meash <m4rtinsch@gmail.com>
2016-05-14 23:46:44 -0400

    compute certain occlusion levels only

--- hmax/computeFeaturesOccluded.m
+++ hmax/computeFeaturesOccluded.m
@@ -3,21 +3,26 @@ function computeFeaturesOccluded()
 dir = fileparts(mfilename('fullpath'));
 addpath([dir '/../data']);
 dataSelection = 1:(325/5):325;
+occlusions = 100:-10:60;
 
-wholeImages = getWholeImages(dataSelection);
+wholeImages = getWholeImages();
 occlusionData = load([dir '/../data/data_occlusion_klab325v2.mat']);
 occlusionData = occlusionData.data;
 
 for selectionIter = 1:length(dataSelection)
     pres = dataSelection(selectionIter);
-    currentIndices = 1:length(occlusionData);
-    currentIndices = currentIndices(occlusionData.pres == pres);
+    indices = 1:length(occlusionData);
+    indices = indices(occlusionData.pres == pres); % current image
+    [~, occlusionIndices] = arrayfun(@(o) ... % closest to occlusion values
+        min(abs(occlusionData.black(indices) - o)), occlusions);
+    indices = indices(occlusionIndices);
     features = struct('row', [], 'pres', [], ...
         'c2', [], 's2', [], 'c1', [], 's1', []);
-    for rowIter = 1:length(currentIndices)
-        row = currentIndices(rowIter);
-        fprintf('%d,%d/%d,%d\n', selectionIter, rowIter, ...
-            length(dataSelection), length(currentIndices));
+    for rowIter = 1:length(indices)
+        row = indices(rowIter);
+        fprintf('%d,%d/%d,%d (%d)\n', selectionIter, rowIter, ...
+            length(dataSelection), length(indices), ...
+            row);
         occludedImage = occlude(wholeImages(pres), row, occlusionData);
         [c2,c1,~,~,s2,s1] = runHmax(occludedImage);
         features(rowIter).row = row;
