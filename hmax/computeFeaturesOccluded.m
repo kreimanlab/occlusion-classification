b5ff5e83c1b4dc156a078a451f9d913199f918cd
Meash <m4rtinsch@gmail.com>
2016-05-14 22:30:56 -0400

    compute features only for first of every category

--- hmax/computeFeaturesOccluded.m
+++ hmax/computeFeaturesOccluded.m
@@ -2,33 +2,32 @@ function computeFeaturesOccluded()
 
 dir = fileparts(mfilename('fullpath'));
 addpath([dir '/../data']);
+dataSelection = 1:(325/5):325;
 
-writeOutEvery = 10;
-
-wholeImages = getWholeImages();
+wholeImages = getWholeImages(dataSelection);
 occlusionData = load([dir '/../data/data_occlusion_klab325v2.mat']);
 occlusionData = occlusionData.data;
 
-features = repmat(struct('c2', [], 's2', [], 'c1', [], 's1', []), ...
-    writeOutEvery, 1);
-totalLength = size(occlusionData, 1);
-featuresIter = 1;
-for dataIter = 1:totalLength
-    fprintf('%d/%d\n', dataIter, totalLength);
-    imageNum = occlusionData.pres(dataIter);
-    occludedImage = occlude(wholeImages(imageNum), dataIter, occlusionData);
-    [c2,c1,~,~,s2,s1] = runHmax(occludedImage);
-    features(featuresIter).c2 = c2;
-    features(featuresIter).c1 = c1;
-    features(featuresIter).s2 = s2;
-    features(featuresIter).s1 = s1;
-    
-    if mod(dataIter, writeOutEvery) == 0 || dataIter == totalLength
-        save([dir '/../data/OcclusionModeling/features/data_occlusion_klab325v2/'...
-            'hmax_all_' num2str(dataIter-writeOutEvery+1) '-' num2str(dataIter) '.mat'], ...
-            '-v7.3', 'features');
-        featuresIter = 1;
-    else
-        featuresIter = featuresIter + 1;
+for selectionIter = 1:length(dataSelection)
+    pres = dataSelection(selectionIter);
+    currentIndices = 1:length(occlusionData);
+    currentIndices = currentIndices(occlusionData.pres == pres);
+    features = struct('row', [], 'pres', [], ...
+        'c2', [], 's2', [], 'c1', [], 's1', []);
+    for rowIter = 1:length(currentIndices)
+        row = currentIndices(rowIter);
+        fprintf('%d,%d/%d,%d\n', selectionIter, rowIter, ...
+            length(dataSelection), length(currentIndices));
+        occludedImage = occlude(wholeImages(pres), row, occlusionData);
+        [c2,c1,~,~,s2,s1] = runHmax(occludedImage);
+        features(rowIter).row = row;
+        features(rowIter).pres = pres;
+        features(rowIter).c2 = c2;
+        features(rowIter).c1 = c1;
+        features(rowIter).s2 = s2;
+        features(rowIter).s1 = s1;
     end
+    save([dir '/../data/OcclusionModeling/features/data_occlusion_klab325v2/'...
+        'hmax_all_' num2str(pres) '.mat'], ...
+        '-v7.3', 'features');
 end
--- hmax/computeFeaturesWhole.m
+++ hmax/computeFeaturesWhole.m
@@ -1,29 +1,24 @@
 function computeFeaturesWhole()
 
-writeOutEvery = 10;
-
 dir = fileparts(mfilename('fullpath'));
 addpath([dir '/../data']);
 
-wholeImages = getWholeImages();
-features = repmat(struct('c2', [], 's2', [], 'c1', [], 's1', []), ...
-    writeOutEvery, 1);
+dataSelection = 1:(325/5):325;
+
+wholeImages = getWholeImages(dataSelection);
 totalLength = length(wholeImages);
-featuresIter = 1;
-for dataIter = 1:totalLength
-    fprintf('%d/%d\n', dataIter, totalLength);
-    [c2,c1,~,~,s2,s1] = runHmax(wholeImages(dataIter));
-    features(featuresIter).c2 = c2;
-    features(featuresIter).c1 = c1;
-    features(featuresIter).s2 = s2;
-    features(featuresIter).s1 = s1;
-    
-    if mod(dataIter, writeOutEvery) == 0 || dataIter == totalLength
-        save([dir '/../data/OcclusionModeling/features/klab325_orig/'...
-            'hmax_all_' num2str(dataIter-writeOutEvery+1) '-' num2str(dataIter) '.mat'], ...
-            '-v7.3', 'features');
-        featuresIter = 1;
-    else
-        featuresIter = featuresIter + 1;
-    end
+features = repmat(struct('pres', [], 'c2', [], 's2', [], 'c1', [], 's1', []), ...
+    totalLength, 1);
+for i = 1:totalLength
+    fprintf('%d/%d\n', i, totalLength);
+    [c2,c1,~,~,s2,s1] = runHmax(wholeImages(i));
+    features(i).pres = dataSelection(i);
+    features(i).c2 = c2;
+    features(i).c1 = c1;
+    features(i).s2 = s2;
+    features(i).s1 = s1;
 end
+selectionStr = num2str(dataSelection, '%d-');
+save([dir '/../data/OcclusionModeling/features/klab325_orig/'...
+    'hmax_all_' selectionStr(1:end-1) '.mat'], ...
+    '-v7.3', 'features');
