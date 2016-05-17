7b009231d77e8662c8e8925cac05d39694528449
Meash <m4rtinsch@gmail.com>
2016-05-17 11:43:29 -0400

    prepare figure for pdf landscape output

--- visualize/displayResults.m
+++ visualize/displayResults.m
@@ -3,6 +3,11 @@ function displayResults(percentsVisible, results)
 accuracies = 100 * reshape([results.accuracy], size(results));
 classifierNames = {results(1, :, 1).name};
 figure();
+% rotate to properly display on pdf
+orient portrait;
+set(gcf, 'papersize', [11 8.5]);
+set(gcf, 'paperposition', [.25 .25 10.5 8]);
+% plot
 hold on;
 xlim([min(percentsVisible)-3, max(percentsVisible)+3]);
 ylim([0 100]);
