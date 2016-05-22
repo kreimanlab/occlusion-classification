6faaabe33569d85f4f3c4d8cbcba1f100a34e6b4
Meash <m4rtinsch@gmail.com>
2016-05-21 23:16:56 -0400

    Implement LibsvmClassifier

---
+++ classify/LibsvmClassifier.m
@@ -0,0 +1,22 @@
+classdef LibsvmClassifier < Classifier
+    %LIBSVMCLASSIFIER Libsvm One-vs-all SVM Classifier.
+    
+    properties
+        classifier
+    end
+    
+    methods
+        function obj = LibsvmClassifier(featureExtractor)
+            obj@Classifier(featureExtractor);
+        end
+        
+        function fit(self, X, Y)
+            self.classifier = libsvmtrain(Y, X, '-q');
+        end
+        
+        function Y = classify(self, X)
+            Y = libsvmpredict(zeros(size(X, 1), 1), ...
+                X, self.classifier, '-q');
+        end
+    end
+end
