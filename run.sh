386f40e6784830e849714f8dd89c3d988c92ed60
Meash <m4rtinsch@gmail.com>
2016-05-24 16:13:04 -0400

    adjust dataMax to filtered dataset

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-bsub -J classification -R "rusage[mem=16000]" -W 96:0 -q priority -o $(date +%Y-%m-%d_%H:%M:%S).out -e $(date +%Y-%m-%d_%H:%M:%S).err matlab -nojvm -nodisplay -r "runClassification('dataMin', 1, 'dataMax', 13000, 'hopSize', 1000);exit"
+bsub -J classification -R "rusage[mem=16000]" -W 96:0 -q priority -o $(date +%Y-%m-%d_%H:%M:%S).out -e $(date +%Y-%m-%d_%H:%M:%S).err matlab -nojvm -nodisplay -r "runClassification('dataMin', 1, 'dataMax', 12600, 'hopSize', 1000);exit"
 
