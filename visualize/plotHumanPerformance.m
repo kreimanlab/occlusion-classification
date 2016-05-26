ab1dba37d2961fffe05aebeabde7004aa51f03d7
Meash <m4rtinsch@gmail.com>
2016-05-26 13:34:36 -0400

    implement statsAcrossAll plot

--- visualize/plotHumanPerformance.m
+++ visualize/plotHumanPerformance.m
@@ -1,27 +1,55 @@
-function plotHumanPerformance(percentsVisible)
+function plotHumanPerformance(percentsBlack)
+percentsBlack = sort(percentsBlack);
 dataset = load('data/data_occlusion_klab325v2.mat');
 dataset = dataset.data;
 dataset = dataset(...
     dataset.pres <= 300 & ...
     dataset.masked == 0 & ...
-    dataset.soa == .15, :);
+    dataset.soa == .15 & ...
+    dataset.occluded == 1, :);
+[meanValues, standardErrorOfTheMean] = statsAcrossAll(dataset, percentsBlack);
+errorbar(permute(100 - percentsBlack, [2 1]), ...
+    meanValues, standardErrorOfTheMean, 'o-');
+text(100 - percentsBlack(1) + 1, meanValues(1), 'human');
+end
+
+function [meanValues, standardErrorOfTheMean] = ...
+    statsAcrossAll(dataset, percentsBlack)
+% averages across all subjects at once
+meanValues = NaN(length(percentsBlack)-1, 1);
+standardErrorOfTheMean = NaN(length(percentsBlack)-1, 1);
+for iPb = 1:length(percentsBlack)
+    currentData = dataset(...
+        dataset.black >= percentsBlack(iPb), :);
+    if iPb < length(percentsBlack)
+        currentData = currentData(...
+            currentData.black < percentsBlack(iPb + 1), :);
+    end
+    meanValues(iPb) = 100 * mean(currentData.correct);
+    standardErrorOfTheMean(iPb) = 100 * ...
+        std(currentData.correct) / sqrt(length(currentData));
+end
+end
+
+function [meanValues, standardErrorOfTheMean] = ...
+    statsAcrossSubjects(dataset, percentsBlack)
+% averages across each subject individually
 subjects = unique(dataset.subject);
-accuracies = zeros(length(percentsVisible), length(subjects));
-visibilityMargin = (percentsVisible(2) - percentsVisible(1)) / 2;
-for iPv = 1:length(percentsVisible)
-    percentBlack = 100 - percentsVisible(iPv);
+accuracies = zeros(length(percentsBlack)-1, length(subjects));
+for iPb = 1:length(percentsBlack)
     for iS = 1:length(subjects)
-        dataSubset = dataset(...
+        currentData = dataset(...
             dataset.subject == subjects(iS) & ...
-            dataset.black >= percentBlack - visibilityMargin & ...
-            dataset.black < percentBlack + visibilityMargin, :);
-        accuracies(iPv, iS) = 100 * ...
-            sum(dataSubset.correct) / length(dataSubset);
+            dataset.black >= percentsBlack(iPb), :);
+        if iPb < length(percentsBlack)
+            currentData = currentData(...
+                currentData.black < percentsBlack(iPb + 1), :);
+        end
+        accuracies(iPb, iS) = 100 * ...
+            mean(currentData.correct);
     end
 end
 meanValues = mean(accuracies, 2, 'omitnan');
 standardErrorOfTheMean = std(accuracies, 0, 2, 'omitnan') / ...
     sqrt(size(accuracies, 2));
-errorbar(permute(percentsVisible, [2 1]), ...
-    meanValues, standardErrorOfTheMean, 'o-');
-text(percentsVisible(end) + 1, meanValues(end), 'human');
+end
