128aea288035c89acb37cf25dd1b19a7e72a7f3f
Meash <m4rtinsch@gmail.com>
2016-06-01 10:21:05 -0400

    use top-variance features for grouped feature visualization

--- visualize/features/alexnet_bipolar_features.m
+++ visualize/features/alexnet_bipolar_features.m
@@ -2,9 +2,11 @@ function alexnet_bipolar_features
 dataset = load('data/data_occlusion_klab325v2.mat');
 dataset = dataset.data(dataset.data.pres <= 300, :);
 
-wholeFeatures = load('data/features/klab325_orig/caffenet_fc7-bipolar0-hop_1-325.mat');
-wholeFeatures = wholeFeatures.features(1:300, :);
-wholeFeatures = downsampleFeatures(1000, wholeFeatures)';
+wholeFeatures = dlmread(['data/features/klab325_orig/'...
+    'caffenet_fc7_ims_1-325.txt'], ' ', 0, 1);
+wholeFeatures = wholeFeatures(1:300, :);
+wholeFeatures = bipolarize(wholeFeatures, 0);
+wholeFeatures = downsampleVar(1000, wholeFeatures)';
 wholeRows = getRows(dataset, (1:size(wholeFeatures, 2))', true);
 wholeLabels = dataset.truth(wholeRows);
 sizeBins = size(wholeFeatures, 2) / length(unique(wholeLabels));
@@ -18,12 +20,15 @@ colorbar;
 colormap(flipud(gray));
 line(repmat((1:numBins-1)' * sizeBins, [1 2]), ...
     [0 size(wholeFeatures, 1)], 'Color', 'red');
+title('Plain features');
 
 subplot(1, 3, 2);
 accumulate(wholeFeatures, wholeLabels, @mean);
+title('mean');
 
 subplot(1, 3, 3);
 accumulate(wholeFeatures, wholeLabels, @median);
+title('median');
 end
 
 function accumulate(wholeFeatures, wholeLabels, fnc)
@@ -37,7 +42,15 @@ colorbar;
 colormap(flipud(gray));
 end
 
-function downsampledFeatures = downsampleFeatures(downsampledLength, features)
+
+function downsampledFeatures = downsampleVar(downsampledLength, features)
+    variances = var(features, 1);
+    [~, sortedIndices] = sort(variances, 'descend');
+    selectedFeatureIndices = sortedIndices(1:downsampledLength);
+    downsampledFeatures = features(:, selectedFeatureIndices);
+end
+
+function downsampledFeatures = downsampleFeaturesNth(downsampledLength, features)
 sampleSteps = ceil(size(features, 2) / downsampledLength);
 downsampledFeatures = zeros(size(features, 1), ...
     ceil(size(features, 2) / sampleSteps));
