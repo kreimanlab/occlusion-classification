db35d437412cd8d7f094cc415c8c9f9c4c24226d
Meash <m4rtinsch@gmail.com>
2016-06-02 16:12:52 -0400

    enable hoptimes and hopsizes in same function

--- visualize/displayResultsHop.m
+++ visualize/displayResultsHop.m
@@ -1,4 +1,5 @@
-function displayResultsHopsizes(results, percentBlackMin, percentBlackMax)
+function displayResultsHop(type, results, ...
+    percentBlackMin, percentBlackMax)
 %HOPSIZE Performance for different hopsizes
 if ~exist('percentBlackMin', 'var')
     percentBlackMin = 75;
@@ -7,12 +8,20 @@ if ~exist('percentBlackMax', 'var')
     percentBlackMax = 80;
 end
 
+if strcmp(type, 'size')
+    pattern = '\-hop([0-9]+)';
+elseif strcmp(type, 'time')
+    pattern = '\-hop_t([0-9]+)';
+else
+    error(['Unknown type ' type]);
+end
+
 %% Stats
 kfolds = length(results);
 classifierNames = unique(results{1}.name);
 hopsizes = zeros(length(classifierNames), 1);
 for i = 1:length(hopsizes)
-    token = regexp(classifierNames{i}, '\-hop([0-9]+)', 'tokens');
+    token = regexp(classifierNames{i}, pattern, 'tokens');
     hopsizes(i) = str2num(token{1}{1});
 end
 accuracies = collectAccuracies(results, ...
@@ -26,7 +35,11 @@ standardErrorOfTheMean = std(accuracies, 0, dimKfolds, 'omitnan') / ...
 xye = [hopsizes, meanValues, standardErrorOfTheMean];
 xye = sortrows(xye, 1);
 errorbar(xye(:, 1), xye(:, 2), xye(:, 3), 'o-');
-xlabel('Hopfield size');
+if strcmp(type, 'size')
+    xlabel('Hopfield size');
+elseif strcmp(type, 'time')
+    xlabel('Timestep');
+end
 ylabel(['Performance with ' num2str(100 - percentBlackMax) ...
     ' - ' num2str(100 - percentBlackMin) '% visibility']);
 end
