e108373c55d85a2be91080dbb052cd7e1ed70d21
Meash <m4rtinsch@gmail.com>
2016-06-03 15:24:50 -0400

    implement RNN functionality, add random weight version

--- feature_extractors/rnn/RnnFeatures.m
+++ feature_extractors/rnn/RnnFeatures.m
@@ -1,15 +1,37 @@
 classdef RnnFeatures < FeatureExtractor
-    % Feature extractor for a RNN layer on top of other features
+    %RNNFEATURES Feature extractor for a RNN layer on top of other features
+    % output_{t+1} = activation(previousFeatures + W .* output_t)
     
+    properties
+        timesteps
+        featuresInput
+        weights
+        activation = @(x) max(0, x)
+    end
+        
     methods
-        function obj = RnnFeatures()
+        function obj = RnnFeatures(timesteps, featuresInput)
+            obj.timesteps = timesteps;
+            obj.featuresInput = featuresInput;
         end
         
         function name = getName(~)
-            name = 'rnn';
+            name = 'RNN';
         end
         
-        function features = extractFeatures(~, ~, ~, ~)
+        function previousFeatures = extractFeatures(self, rows, runType, labels)
+            previousFeatures = self.featuresInput.extractFeatures(rows, runType, labels);
+            if isempty(self.weights)
+                self.weights = self.createWeights(size(previousFeatures, 2));
+            end
+            W = repmat(self.weights, size(previousFeatures, 1), 1);
+            features = previousFeatures;
+            for t = 1:self.timesteps
+                features = self.activation(previousFeatures + W .* features);
+            end
+        end
+        
+        function weights = createWeights(~, ~)
             error('Not implemented');
         end
     end
