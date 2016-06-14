6a160110eae011d97bc41d4466518732db4c46a4
Meash <m4rtinsch@gmail.com>
2016-06-14 13:34:32 -0400

    use closest occlusion from same pres image

--- data/occludeBlack.m
+++ data/occludeBlack.m
@@ -1,9 +1,11 @@
-function occludedImage = occludeBlack(imageNum, black)
+function occludedImage = occludeBlack(pres, black)
 image = load('data/KLAB325.mat');
-image = image.img_mat{imageNum};
+image = image.img_mat{pres};
 data = load('data/data_occlusion_klab325v2.mat');
 data = data.data;
+data = data(data.pres == pres, :);
 [~, idx] = min(abs(data.black - black));
+fprintf('Using %.2f%% occlusion\n', data.black(idx));
 bub_sig = 14;
 numBubbles = data.nbubbles(idx);
 S.c = data.bubble_centers(idx, 1:numBubbles);
