9398d159621e241b1538319fe1985d3b5bb59268
Meash <m4rtinsch@gmail.com>
2016-07-20 14:54:59 -0400

    cross-validate RNN across objects, store features in local directory

--- feature_extractors/rnn/RnnFeatures.py
+++ feature_extractors/rnn/RnnFeatures.py
@@ -2,44 +2,52 @@ import os
 import random
 
 import numpy as np
+from sklearn.cross_validation import KFold
 from keras.layers import SimpleRNN
 from keras.models import Sequential
 
 
-def create_model():
+def create_model(feature_size):
     model = Sequential()
     model.add(SimpleRNN(output_dim=feature_size, input_shape=(None, feature_size),
                         activation='relu',
-                        # fixed_W=True,
-                        return_sequences=False, name='RNN'))
+                        return_sequences=True, name='RNN'))
     model.compile(loss="mse", optimizer="rmsprop", metrics=["accuracy"])
     return model
 
 
 def train(model, X, Y, num_epochs=10, batch_size=512, validation_split=0.1):
     assert len(X) == len(Y)
-    model.fit(reshape_x(X), Y,
-              batch_size=batch_size, nb_epoch=num_epochs, validation_split=validation_split,
-              verbose=1)
-    model.save_weights('model_weights.hdf5', overwrite=True)
-    return model
+    initial_weights = model.get_weights()
+    validation_losses = []
+    for epoch in range(num_epochs):
+        print('Epoch %d' % epoch)
+        metrics = model.fit(reshape_features(X), reshape_features(Y),
+                            batch_size=batch_size, nb_epoch=1, validation_split=validation_split, verbose=0)
+        validation_losses.append(metrics.history['val_loss'][0])
+    best_epoch = np.array(validation_losses).argmin()
+    print('retraining on whole data up to best validation epoch %d' % best_epoch)
+    model.reset_states()
+    model.set_weights(initial_weights)
+    model.fit(reshape_features(X), reshape_features(Y),
+              batch_size=batch_size, nb_epoch=best_epoch, verbose=1)
 
 
-def reshape_x(x):
-    return np.resize(x, [x.shape[0], 1, x.shape[1]])
+def reshape_features(features, timesteps=1):
+    features = np.resize(features, [features.shape[0], 1, features.shape[1]])
+    return np.repeat(features, timesteps, 1)
 
 
-def run(model, X, num_timesteps=6):
+def run(model, X, timesteps=6):
     Y = {}
-    for t in range(0, num_timesteps):
-        if t == 0:
-            Y[t] = X
-        else:
-            Y[t] = model.predict(reshape_x(X))
+    Y[0] = X
+    predictions = model.predict(reshape_features(X, timesteps))
+    for t in range(1, timesteps + 1):
+        Y[t] = predictions[:, t - 1, :]
     return Y
 
 
-def load_occluded_features(features_directory):
+def load_occluded_features(features_directory, feature_size):
     features = np.zeros((13000, feature_size))
     for i in range(1, features.shape[0], 1000):
         filename = os.path.join(features_directory, "data_occlusion_klab325v2",
@@ -49,7 +57,7 @@ def load_occluded_features(features_directory):
     return features
 
 
-def load_whole_features(features_directory):
+def load_whole_features(features_directory, feature_size):
     filename = os.path.join(features_directory, "klab325_orig",
                             "caffenet_fc7_ims_1-325.txt")
     features = np.loadtxt(filename, usecols=range(1, feature_size + 1))
@@ -67,32 +75,65 @@ def align_features(whole_features, occluded_features):
     return aligned_whole
 
 
-def get_features_directory():
+def get_features_directory(use_central=True):
     central_dir = "/groups/kreiman/martin/features"
-    if os.path.exists(central_dir):
+    if use_central and os.path.exists(central_dir):
         return central_dir
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../data/features")
 
 
-def load_data():
+def cross_validate_prediction(model, X, Y, kfolds, train_epochs, max_timestep):
+    """
+    for each kfold, train on subset of features and predict the rest.
+    Ultimately predict all features by concatenating them for each kfold.
+    """
+    initial_model_weights = model.get_weights()
+    predicted_features = np.zeros((max_timestep + 1,) + X.shape)
+    num_kfold = 0
+    for train_indices, predict_indices in kfolds:
+        model.reset_states()
+        model.set_weights(initial_model_weights)
+
+        X_train, Y_train = X[train_indices], Y[train_indices]
+        X_predict, Y_predict = X[predict_indices], Y[predict_indices]
+        print('[kfold %d] training...' % num_kfold)
+        train(model, X_train, Y_train, num_epochs=train_epochs)
+        model.save_weights('model_weights-kfold%d.hdf5' % num_kfold, overwrite=True)
+        print('[kfold %d] predicting...' % num_kfold)
+        predicted_Y = run(model, X_predict, timesteps=max_timestep)
+        for timestep, prediction in predicted_Y.items():
+            predicted_features[timestep, predict_indices, :] = prediction
+
+        num_kfold += 1
+    return predicted_features
+
+
+def run_rnn():
+    # params
+    feature_size = 4096
+    num_kfolds = 5
+    num_epochs = 50
+    max_timestep = 6
+    # init model
+    model = create_model(feature_size)
+    # load data
     features_directory = get_features_directory()
-    Y = load_whole_features(features_directory)
-    X = load_occluded_features(features_directory)
-    Y = align_features(Y, X)
-    return X, Y
+    whole_features = load_whole_features(features_directory, feature_size)
+    occluded_features = load_occluded_features(features_directory, feature_size)
+    aligned_whole_features = align_features(whole_features, occluded_features)
+    # run
+    kfolds = KFold(occluded_features.shape[0], num_kfolds)
+    predicted_features = cross_validate_prediction(model, occluded_features, aligned_whole_features,
+                                                   kfolds, train_epochs=num_epochs, max_timestep=max_timestep)
+    # save
+    print('saving...')
+    for timestep in range(0, max_timestep + 1):
+        features = predicted_features[timestep]
+        filename = 'RnnFeatures-timestep%d.txt' % timestep
+        filepath = os.path.join(get_features_directory(False), filename)
+        np.savetxt(filepath, features)
 
 
 if __name__ == '__main__':
     random.seed(0)
-    feature_size = 4096
-    model = create_model()
-    # data
-    X, Y = load_data()
-    # TODO: cross validation across objects/categories
-    model = train(model, X, Y, num_epochs=30)
-    predicted_Y = run(model, X)
-    for timestep, prediction in predicted_Y.items():
-        features = np.concatenate((Y, prediction))
-        filename = 'RnnFeatures-timestep%d.txt' % timestep
-        filepath = os.path.join(get_features_directory(), filename)
-        np.savetxt(filepath, prediction)
+    run_rnn()
