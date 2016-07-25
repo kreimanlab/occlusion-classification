6e5044722e940e4435a29c2b2a1fc5262b04c55c
Meash <m4rtinsch@gmail.com>
2016-07-25 16:29:25 -0400

    same whole features within a model

--- visualize/features/plotWholeVsOccludedTsne.m
+++ visualize/features/plotWholeVsOccludedTsne.m
@@ -1,12 +1,16 @@
-function figures = plotWholeVsOccludedTsne(featureExtractors, figurePrefix)
+function figures = plotWholeVsOccludedTsne(...
+    featureExtractors, figurePrefix)
 
 % data
 dataset = load('data/data_occlusion_klab325v2.mat');
 dataset = dataset.data;
-dataSelection = 1:13000;
+dataSelection = 1:size(dataset, 1);
+featureProviderFactory = FeatureProviderFactory(dataset, dataSelection);
 % selection
 [~, rows] = unique(dataset.pres(:)); % pick one version of each object
 rows = rows';
+% labels(whole) == labels(occluded) since they belong to the same category
+labels = dataset.truth(rows);
 % feature extractors
 if ~exist('featureExtractors', 'var')
     featureProviderFactory = FeatureProviderFactory(dataset, dataSelection);
@@ -20,16 +24,39 @@ end
 [numRows, numCols] = size(featureExtractors);
 figures = NaN(numRows * numCols, 1);
 for featIter = 1:numRows
+    wholeFeatures = NaN(325, 4096);
+    occludedFeatures = NaN(numel(rows) * numCols, 4096);
     % show over time
     for timeIter = 1:numCols
-        rng(0, 'twister');
         extractor = featureExtractors{featIter, timeIter};
-        fprintf('%s (%d.%d/%d.%d)\n', extractor.getName(), ...
+        providedExtractor = featureProviderFactory.get(extractor);
+        fprintf('%s (%d.%d/%d.%d)\n', providedExtractor.getName(), ...
             featIter, timeIter, numRows, numCols);
-        figures((featIter - 1) * numCols + timeIter) = ...
-            figure('Name', [figurePrefix, '-', extractor.getName()]);
-        plotTsne(extractor, rows, dataset, colors);
-        if (timeIter == 1)
+        if timeIter == 1
+            wholeFeatures(:) = ...
+                providedExtractor.extractFeatures(rows, RunType.Train);
+        end
+        occludedFeatures((timeIter - 1) * numel(rows) + 1:...
+            timeIter * numel(rows), :) = ...
+            providedExtractor.extractFeatures(rows, RunType.Test);
+        % we usually don't need the extractor again, free up memory space
+        featureProviderFactory.remove(extractor);
+    end
+    assert(~any(isnan(occludedFeatures(:))));
+    % do tsne
+    rng(0, 'twister');
+    mappedX = tsne([wholeFeatures; occludedFeatures], [], 2);
+    mappedXWhole = mappedX(1:size(wholeFeatures, 1), :);
+    % plot
+    for timeIter = 1:numCols
+        extractor = featureExtractors{featIter, timeIter};
+        mappedXOccluded = mappedX(...
+            size(wholeFeatures, 1) + (timeIter - 1) * numel(rows) + 1:...
+            size(wholeFeatures, 1) + timeIter * numel(rows), :);
+        figures((featIter - 1) * numCols + timeIter) = figure('Name', ...
+            sprintf('%s%d-%s', figurePrefix, timeIter, extractor.getName()));
+        plotTsne(mappedXWhole, mappedXOccluded, labels, colors, extractor);
+        if timeIter == 1
             makeLegend(labelNames, colors);
         end
     end
@@ -37,26 +64,18 @@ for featIter = 1:numRows
 end
 end
 
-function plotTsne(extractor, rows, dataset, colors)
-rng(0, 'twister');
-wholeFeatures = extractor.extractFeatures(rows, RunType.Train);
-occludedFeatures = extractor.extractFeatures(rows, RunType.Test);
-labels = dataset.truth(rows);
-mappedX = tsne([wholeFeatures', occludedFeatures']', [], 2);
-mappedXWhole = mappedX(1:size(wholeFeatures, 1), :);
-mappedXOccluded = mappedX(size(wholeFeatures, 1) + 1:end, :);
-% plot x, y
-set(gca, 'XTickLabel', ''); set(gca, 'YTickLabel', '');
-hold on;
-categoryLabels = getCategoryLabels(labels);
-gscatter(mappedXOccluded(:, 1), mappedXOccluded(:, 2), ...
-    categoryLabels, cell2mat(colors), '.', 10, false);
-gscatter(mappedXWhole(:, 1), mappedXWhole(:, 2), ...
-    categoryLabels, cell2mat(colors), 'o', 5, false);
-title(strrep(extractor.getName(), '_', '\_'));
-set(gca, 'box', 'off');
-axis off;
-hold off;
+function plotTsne(mappedXWhole, mappedXOccluded, labels, colors, extractor)
+    categoryLabels = getCategoryLabels(labels);
+    gscatter(mappedXOccluded(:, 1), mappedXOccluded(:, 2), ...
+        categoryLabels, cell2mat(colors), '.', 10, false);
+    hold on;
+    gscatter(mappedXWhole(:, 1), mappedXWhole(:, 2), ...
+        categoryLabels, cell2mat(colors), 'o', 5, false);
+    title(strrep(extractor.getName(), '_', '\_'));
+    set(gca, 'XTickLabel', ''); set(gca, 'YTickLabel', '');
+    set(gca, 'box', 'off');
+    axis off;
+    hold off;
 end
 
 function makeLegend(labels, colors)
