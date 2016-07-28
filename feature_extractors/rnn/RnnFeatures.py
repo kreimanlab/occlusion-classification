7093ffaf27438b037d6fed5e380139b46a013da3
Meash <m4rtinsch@gmail.com>
2016-07-28 09:23:04 -0400

    fix indices across objects, shuffle training data, add whole indices

--- feature_extractors/rnn/RnnFeatures.py
+++ feature_extractors/rnn/RnnFeatures.py
@@ -2,6 +2,7 @@ import os
 import random
 
 import numpy as np
+import scipy.io
 from keras.layers import SimpleRNN
 from keras.models import Sequential
 from sklearn.cross_validation import KFold
@@ -17,7 +18,9 @@ def create_model(feature_size):
 
 
 def train(model, X, Y, num_epochs=10, batch_size=512, validation_split=0.1):
-    assert len(X) == len(Y)
+    assert len(X) == len(Y), \
+        "len(X) = %d and len(Y) = %d do not match" % (len(X), len(Y))
+    X, Y = shuffle(X, Y)
     initial_weights = model.get_weights()
     validation_losses = []
     for epoch in range(num_epochs):
@@ -33,12 +36,19 @@ def train(model, X, Y, num_epochs=10, batch_size=512, validation_split=0.1):
               batch_size=batch_size, nb_epoch=best_epoch, verbose=1)
 
 
+def shuffle(X, Y):
+    XY = list(zip(X, Y))
+    random.shuffle(XY)
+    X, Y = zip(*XY)
+    return X, Y
+
+
 def reshape_features(features, timesteps=1):
     features = np.resize(features, [features.shape[0], 1, features.shape[1]])
     return np.repeat(features, timesteps, 1)
 
 
-def run(model, X, timesteps=6):
+def predict(model, X, timesteps=6):
     Y = {}
     Y[0] = X
     predictions = model.predict(reshape_features(X, timesteps))
@@ -48,13 +58,14 @@ def run(model, X, timesteps=6):
 
 
 def load_occluded_features(features_directory, feature_size):
-    num_features = 13325
+    num_features = 13000
     features = np.zeros((num_features, feature_size))
-    for i in range(1, num_features, 1000):
+    for range_lower in range(1, num_features, 1000):
+        range_upper = min(range_lower + 999, num_features)
         filename = os.path.join(features_directory, "data_occlusion_klab325v2",
-                                "caffenet_fc7_ims_%d-%d.txt" % (i, min(i + 999, num_features)))
+                                "caffenet_fc7_ims_%d-%d.txt" % (range_lower, range_upper))
         _features = np.loadtxt(filename, usecols=range(1, feature_size + 1))
-        features[i - 1:i - 1 + 1000, :] = _features
+        features[range_lower - 1:range_upper, :] = _features
     return features
 
 
@@ -65,10 +76,9 @@ def load_whole_features(features_directory, feature_size):
     return features
 
 
-def align_features(whole_features, occluded_features):
-    pres = np.loadtxt(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                   "../../data/data_occlusion_klab325v2-pres.txt"))
-    assert len(pres) == len(occluded_features)
+def align_features(whole_features, occluded_features, pres):
+    assert len(pres) == len(occluded_features), \
+        "len(pres) = %d and len(occluded_features) = %d do not match" % (len(pres), len(occluded_features))
     aligned_whole = np.zeros(occluded_features.shape)
     for i in range(len(occluded_features)):
         corresponding_whole = int(pres[i])
@@ -95,27 +105,53 @@ def get_weights_file(kfold):
     return weights_file
 
 
-def indices_across_objects(occluded_features, num_kfolds):
+def indices_across_images(occluded_features, num_kfolds):
     return KFold(occluded_features.shape[0], num_kfolds)
+
+
+def indices_across_objects(occluded_features, pres, num_kfolds):
+    assert len(pres) == len(occluded_features), \
+        "len(pres) = %d and len(occluded_features) = %d do not match" % (len(pres), len(occluded_features))
+    unique_pres = list(set(pres))
+    kfold = KFold(len(unique_pres), num_kfolds)
+    for train_object_indices, predict_object_indices in kfold:
+        train_objects = [pres[i] for i in train_object_indices]
+        predict_objects = [pres[i] for i in predict_object_indices]
+        train_indices = [i for i in range(len(pres))
+                         if pres[i] in train_objects and i < occluded_features.shape[0]]
+        predict_indices = [i for i in range(len(pres))
+                           if pres[i] in predict_objects and i < occluded_features.shape[0]]
+        assert all([pres[i] in train_objects for i in train_indices]), "train objects outside range"
+        assert all([pres[i] in predict_objects for i in predict_indices]), "predict objects outside range"
+        yield (train_indices, predict_indices)
 
 
 def indices_across_categories(occluded_features, num_kfolds):
     categories = np.loadtxt(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                         "../../data/data_occlusion_klab325v2-categories.txt"))
-    assert len(categories) == len(occluded_features)
+                                         "../../data/data_occlusion_klab325v2-categories.txt"), dtype='int')
+    assert len(categories) == len(occluded_features), \
+        "len(categories) = %d and len(occluded_features) = %d do not match" % (len(categories), len(occluded_features))
     unique_categories = list(set(categories))
-    assert num_kfolds == len(unique_categories)
+    assert num_kfolds == len(unique_categories), \
+        "num_kfolds = %d and len(unique_categories) = %d do not match" % (num_kfolds, len(unique_categories))
     kfold = KFold(len(unique_categories), num_kfolds)
     for train_category_indices, predict_category_indices in kfold:
         train_categories = [unique_categories[i] for i in train_category_indices]
         predict_categories = [unique_categories[i] for i in predict_category_indices]
         train_indices = [i for i in range(len(categories))
-                         if categories[i] in train_categories and i < len(occluded_features)]
+                         if categories[i] in train_categories and i < occluded_features.shape[0]]
         predict_indices = [i for i in range(len(categories))
-                           if categories[i] in predict_categories and i < len(occluded_features)]
-        assert all([categories[i] in train_categories for i in train_indices])
-        assert all([categories[i] in predict_categories for i in predict_indices])
+                           if categories[i] in predict_categories and i < occluded_features.shape[0]]
+        assert all([categories[i] in train_categories for i in train_indices]), "train categories outside range"
+        assert all([categories[i] in predict_categories for i in predict_indices]), "predict categories outside range"
         yield (train_indices, predict_indices)
+
+
+def add_whole_indices(rows, pres):
+    for train_indices, test_indices in rows:
+        test_pres = list(set(pres[test_indices]))
+        test_indices_with_whole = np.concatenate(([i + 325 for i in test_indices], test_pres))
+        yield (train_indices, test_indices_with_whole)
 
 
 def cross_validate_prediction(model, X, Y, rows, train_epochs, max_timestep):
@@ -131,7 +167,7 @@ def cross_validate_prediction(model, X, Y, rows, train_epochs, max_timestep):
         model.set_weights(initial_model_weights)
 
         X_train, Y_train = X[train_indices], Y[train_indices]
-        X_predict, Y_predict = X[predict_indices], Y[predict_indices]
+        X_predict = X[predict_indices]
         weights_file = get_weights_file(num_kfold)
         if os.path.isfile(weights_file):
             print('[kfold %d] using pre-trained weights %s' % (num_kfold, weights_file))
@@ -141,7 +177,7 @@ def cross_validate_prediction(model, X, Y, rows, train_epochs, max_timestep):
             train(model, X_train, Y_train, num_epochs=train_epochs)
             model.save_weights(weights_file, overwrite=True)
         print('[kfold %d] predicting...' % num_kfold)
-        predicted_Y = run(model, X_predict, timesteps=max_timestep)
+        predicted_Y = predict(model, X_predict, timesteps=max_timestep)
         for timestep, prediction in predicted_Y.items():
             predicted_features[timestep, predict_indices, :] = prediction
 
@@ -153,27 +189,34 @@ def run_rnn():
     # params
     feature_size = 4096
     num_kfolds = 5
-    num_epochs = 50
+    num_epochs = 100
     max_timestep = 6
-    # init model
+    # model
     model = create_model(feature_size)
     # load data
     features_directory = get_features_directory()
     whole_features = load_whole_features(features_directory, feature_size)
     occluded_features = load_occluded_features(features_directory, feature_size)
-    aligned_whole_features = align_features(whole_features, occluded_features)
+    pres = np.loadtxt(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                   "../../data/data_occlusion_klab325v2-pres.txt"), dtype='int')
+    aligned_whole_features = align_features(whole_features, occluded_features, pres)
     # run
-    # row_provider = indices_across_objects(occluded_features, num_kfolds)
+    X = np.concatenate((whole_features, occluded_features))
+    Y = aligned_whole_features
+    # row_provider = indices_across_images(occluded_features, num_kfolds)
+    # row_provider = indices_across_objects(occluded_features, pres, num_kfolds)
     row_provider = indices_across_categories(occluded_features, num_kfolds)
-    predicted_features = cross_validate_prediction(model, occluded_features, aligned_whole_features,
-                                                   row_provider, train_epochs=num_epochs, max_timestep=max_timestep)
+    row_provider = add_whole_indices(row_provider, pres)
+    predicted_features = cross_validate_prediction(model, X, Y, row_provider,
+                                                   train_epochs=num_epochs, max_timestep=max_timestep)
     # save
     print('saving...')
     for timestep in range(0, max_timestep + 1):
         features = predicted_features[timestep]
-        filename = 'RnnFeatures-timestep%d.txt' % timestep
+        # save
+        filename = 'RnnFeatures-timestep%d' % timestep
         filepath = os.path.join(get_features_directory(False), filename)
-        np.savetxt(filepath, features)
+        scipy.io.savemat(filepath, {'features': features})
 
 
 if __name__ == '__main__':
