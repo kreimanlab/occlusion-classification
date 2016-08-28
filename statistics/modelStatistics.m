49992101c9cc91120580f4d6194232f1d18ef2d9
Meash <m4rtinsch@gmail.com>
2016-08-28 19:23:05 -0400

    compute means and errors in the same way as the figures do

--- statistics/modelStatistics.m
+++ statistics/modelStatistics.m
@@ -9,30 +9,26 @@ rnnTimes = load('data/results/classification/rnntimes.mat');
 fc7Results = changeResults(filterResults(rnnTimes.results, ...
     @(r) strcmp(r.name, 'RNN_features_fc7_noRelu_t0-libsvmccv')), ...
     'name', @(r) repmat({'fc7'}, [size(r, 1), 1]));
-fc7Results = collapseResults(fc7Results);
 rnn5Results = changeResults(filterResults(rnnTimes.results, ...
     @(r) strcmp(r.name, 'RNN_features_fc7_noRelu_t4-libsvmccv')), ...
     'name', @(r) repmat({'RNN5'}, [size(r, 1), 1]));
-rnn5Results = collapseResults(rnn5Results);
 % hop
 hopResults = load('data/results/classification/hoptimes-trainAll.mat');
 hopResults = changeResults(filterResults(hopResults.results, ...
     @(r) strcmp(r.name, 'caffenet_fc7-bipolar0-hop_t256-libsvmccv')), ...
     'name', @(r) repmat({'RNNH'}, [size(r, 1), 1]));
-hopResults = collapseResults(hopResults);
 % RNN1
 rnn1Results = load('data/results/classification/train1cat/excludeTrainCategory/merged.mat');
 rnn1Results = changeResults(filterResults(rnn1Results.results, ...
     @(r) cellfun(@(m) ~isempty(m), ...
     regexp(r.name, 'train1cat/split-[0-5]_t\-4\/features-libsvmccv'))), ...
     'name', @(r) repmat({'RNN1'}, [size(r, 1), 1]));
-rnn1Results = collapseResults(rnn1Results);
 % RNN-masked
 rnnMaskedResults = getRnnMaskedResults();
 
 %% comparisons
 pairs = {hopResults, fc7Results; ...
-    rnn5Results, humanResults; ...
+    rnn5Results, {humanResults}; ...
     rnn1Results, fc7Results};
 for i = 1:numel(rnnMaskedResults)
     pairs(end + 1, :) = {rnnMaskedResults{i}, rnn5Results};
@@ -40,30 +36,35 @@ end
 for row = 1:size(pairs, 1)
     results1 = pairs{row, 1};
     results2 = pairs{row, 2};
-    name1 = unique(results1.name);
-    name2 = unique(results2.name);
-    correct1 = results1.correct;
-    correct2 = results2.correct;
-    overall1 = getAccuracies(results1);
-    overall2 = getAccuracies(results2);
-    [h, p] = chi2(overall1, overall2);
+    name1 = unique(results1{1}.name);
+    name2 = unique(results2{1}.name);
+    accuracies1 = getAccuracies(results1);
+    accuracies2 = getAccuracies(results2);
+    assert(size(accuracies1, 2) == numel(results1));
+    meanAccuracies1 = mean(accuracies1, 2);
+    meanAccuracies2 = mean(accuracies2, 2);
+    [h, p] = chi2(meanAccuracies1, meanAccuracies2);
     fprintf(['[6C] overall %5s: %.0f%% +- %.2f%%, != %5s (%.2f%% +- %.2f%%): ', ...
         'h=%d, p=%d\n'], ...
-        name1{:}, 100 * mean(correct1), 100 * stderrmean(correct1), ...
-        name2{:}, 100 * mean(correct2), 100 * stderrmean(correct2), ...
+        name1{:}, mean(meanAccuracies1), mean(stderrmean(accuracies1, ...
+        ...% last non-singleton dimension
+        find(size(accuracies1) > 1, 1, 'last'))), ...
+        name2{:}, mean(meanAccuracies2), mean(stderrmean(accuracies2, ...
+        ...% last non-singleton dimension
+        find(size(accuracies2) > 1, 1, 'last'))), ...
         h, p);
 end
 end
 
 function accuracies = getAccuracies(results)
-classifierName = unique(results.name);
+classifierName = unique(results{1}.name);
 assert(numel(classifierName) == 1);
 percentsBlack = [65:5:95, 99];
-accuracies = NaN(size(percentsBlack));
+accuracies = NaN([numel(percentsBlack), numel(results)]);
 for iBlack = 1:length(percentsBlack)
     [blackMin, blackMax] = ...
         getPercentBlackRange(percentsBlack, iBlack);
-    accuracies(iBlack) = collectAccuracies({results}, ...
+    accuracies(iBlack, :) = collectAccuracies(results, ...
         blackMin, blackMax, classifierName);
 end
 end
@@ -71,7 +72,7 @@ end
 function rnnMaskedResults = getRnnMaskedResults()
 results = load('data/results/classification/rnn-masking.mat');
 results = results.results;
-masks = 0:4;
+masks = 0:5;
 timestep = 4;
 rnnMaskedResults = cell(size(masks));
 for maskIter = 1:numel(masks)
@@ -80,7 +81,6 @@ for maskIter = 1:numel(masks)
         @(r) cell2mat(cellfun(@(s) ~isempty(s), ...
         strfind(r.name, sprintf('mask%d-rnn-t%d', mask, timestep)), ...
         'UniformOutput', false)));
-    currentResults = joinExperimentData(currentResults);
-    rnnMaskedResults{maskIter} = collapseResults(currentResults);
+    rnnMaskedResults{maskIter} = joinExperimentData(currentResults);
 end
 end
