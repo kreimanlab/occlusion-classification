8879aa2c0370cbca6ab4f01e24653128d2648142
Meash <m4rtinsch@gmail.com>
2017-03-01 00:56:50 +0100

    add plotting RNN_h with different mask onsets
    * delete experiment data vars in join all within function

--- visualize/performance/collectAccuracies.m
+++ visualize/performance/collectAccuracies.m
@@ -1,5 +1,15 @@
 function accuracies = collectAccuracies(results, ...
     percentBlackMin, percentBlackMax, classifierNames)
+if ~exist('percentBlackMin', 'var')
+    percentBlackMin = intmin;
+end
+if ~exist('percentBlackMax', 'var')
+    percentBlackMax = intmax;
+end
+if ~exist('classifierNames', 'var')
+    classifierNames = unique(results{1}.name);
+end
+
 accuracies = NaN(length(classifierNames), length(results));
 for iCls = 1:length(classifierNames)
     for ikfold = 1:length(results)
--- visualize/performance/plotHopMaskingOverTime.m
+++ visualize/performance/plotHopMaskingOverTime.m
@@ -1,18 +1,44 @@
-function plotHopMaskingOverTime(results)
-for maskExp = [-1, 1:6]
-    if maskExp == -1
-        mask = 0;
-        name = 'sumFeatures_alexnet-relu7-bipolar0_alexnet-relu7-masked-bipolar0-hop_t';
-    else
-        mask = 2^maskExp;
-        name = sprintf('-hop_t%d_alexnet-relu7-masked', mask);
-    end
-    currentResults = filterResults(results, ...
-        @(r) cell2mat(cellfun(@(s) ~isempty(s), ...
-        strfind(r.name, name), 'UniformOutput', false)));
-    [~, text] = plotOverallPerformanceOverTime(currentResults);
-    hold on;
-    text = get(text, 'Text');
-    text.String = sprintf('%s-mask%d', text.String, mask);
+function plotHopMaskingOverTime(hopMaskingResults, hopResults256)
+%% load results
+if ~exist('hopResults256', 'var')
+    timeResultsHop = load('data/results/classification/hoptimes-trainAll.mat');
+    timeResultsHop = timeResultsHop.results;
+    hopResults256 = filterResults(timeResultsHop, ...
+        @(r) strcmp(r.name, 'caffenet_fc7-bipolar0-hop_t256-libsvmccv'));
 end
+%% load experiment data
+experimentData = load('data_occlusion_klab325v2.mat');
+experimentData = experimentData.data;
+
+timesteps = [2, 4, 8, 16, 32, 64, 256];
+accuracies = NaN(numel(timesteps), numel(hopMaskingResults));
+for timeIter = 1:numel(timesteps)
+    %% collect
+    t = timesteps(timeIter);
+    if t == 256 % no mask
+        maskingResults = hopResults256;
+    else
+        if t == 0
+            pattern = '^sumFeatures_alexnet-relu7-bipolar0_alexnet-relu7-masked-bipolar0-hop_t256-libsvmccv$';
+        else
+            pattern = ['^.*.bipolar0-hop_t', num2str(t), '_.*-hop_t256-libsvmccv$'];
+        end
+        maskingResults = filterResults(hopMaskingResults, ...
+            @(r) cellfun(@(i) ~isempty(i), regexp(r.name, pattern)));
+        maskingResults = joinExperimentData(maskingResults, experimentData);
+    end
+    assert(sum(cellfun(@(r) size(r, 1), maskingResults)) == 13000);
+    
+    accuracies(timeIter, :) = collectAccuracies(maskingResults);
+end
+%% plot
+performances = mean(accuracies, 2);
+errors = stderrmean(accuracies, 2);
+errorbar(timesteps, performances, errors);
+hold on;
+plot(xlim(), [20, 20], 'k--'); % chance
+xlabel('mask SOA');
+ylabel('% correct');
+ylim([0, 100]);
+hold off;
 end
