cfed09bf80048c63550997793e32ea2e85990372
Martin Schrimpf <m4rtinsch@gmail.com>
2018-01-25 11:00:35 -0500

    handle sub-directories; ignore imread errors; fix cropping

--- data/imagenet/prepareImagenet.m
+++ data/imagenet/prepareImagenet.m
@@ -1,40 +1,69 @@
-function data = prepareImagenet(imagesDirectory, occludedWholeRatio)
+function data = prepareImagenet(imagesDirectory, targetDirectory, occludedWholeRatio)
 %PREPAREIMAGENET prepares the data for fine-tuning (mixed training)
 %   occludedWholeRatio: number of occluded images to train on divided by 
 %   number of whole images to train on
 
+imagesDirectory = char(imagesDirectory); targetDirectory = char(targetDirectory);
 scriptDirectory = fileparts(mfilename('fullpath'));
 assert(exist('imagesDirectory', 'var') == 1);
-targetDirectory = [scriptDirectory, '/processed_images/'];
+if ~exist('targetDirectory', 'var')
+    targetDirectory = [scriptDirectory, '/processed_images/'];
+end
 mkdir(targetDirectory);
+fprintf('running with imagesDirectory=%s, targetDirectory=%s\n', ...
+    imagesDirectory, targetDirectory);
 
 %% settings
 if ~exist('occludedWholeRatio', 'var')
     occludedWholeRatio = 1/1;
 end
+% TODO: this is different from the original 256x256, so make sure to keep
+% occlusion distribution equal
 targetImageSize = [227, 227];
 convertGrayscale = false;
+gray = 128;
 bubblesPerImage = 5;
 bubbleSize = 14;
 fprintf('using bubble size %d\n', bubbleSize);
-logEvery = 1;
-gray = 128;
 rng(0, 'twister');
 
 %% read images
 fprintf('collecting images...\n');
-imageFiles = dir([imagesDirectory, '/*.JPEG']);
-images = cell(size(imageFiles));
-for i = 1:numel(imageFiles)
-    if mod(i - 1, logEvery) == 0 || i == numel(imageFiles)
-        fprintf('%d/%d\n', i, numel(imageFiles));
-    end
-    images{i} = imread([imagesDirectory, '/', imageFiles(i).name]);
-    images{i} = resizedCenterCrop(images{i}, targetImageSize);
-    if convertGrayscale && isRgb(images{i})
-        images{i} = rgb2gray(images{i});
+images = cell(0);
+relativeImagePaths = cell(0);
+count = 0;
+errors = 0;
+subDirectories = dir(imagesDirectory);
+subDirectories = {subDirectories.name};
+subDirectories(ismember(subDirectories, {'.', '..'})) = [];
+for dirCounter = 1:numel(subDirectories)
+    subDirectory = subDirectories{dirCounter};
+    fprintf('Directory %d/%d: %s\n', dirCounter, numel(subDirectories), subDirectory);
+    imagesSubDirectory = [imagesDirectory, '/', subDirectory];
+    imageFiles = dir([imagesSubDirectory, '/*.JPEG']);
+    for i = 1:numel(imageFiles)
+        imagePath = [imagesSubDirectory, '/', imageFiles(i).name];
+        try
+            image = imread(imagePath);
+        catch e
+            msgText = getReport(e);
+            warning(msgText);
+            errors = errors + 1;
+            continue;
+        end
+        image = resizedCenterCrop(image, targetImageSize);
+        if convertGrayscale && isRgb(image)
+            image = rgb2gray(image);
+        end
+        images{end + 1} = image;
+        relativeImagePaths{end + 1} = relativepath(fullfile(pwd, imagePath), fullfile(pwd, imagesDirectory));
+        relativeImagePaths{end} = relativeImagePaths{end}(3:end - 1);
+        count = count + 1;
     end
 end
+images = images'; relativeImagePaths = relativeImagePaths';
+fprintf('Found %d images\n', count);
+fprintf('%d images failed to read\n', errors);
 backgroundMasks = false; % TODO: eventually use bounding box here
 
 %% set up ratio
@@ -58,17 +87,24 @@ disp('occluding images...');
     wholeImagesToOcclude, backgroundMasks, gray, numBubbles, bubbleCenters, bubbleSigmas);
 
 %% save
-fprintf('saving...\n');
+fprintf('saving data...\n');
 black = 100 - imagesVisible;
 bubble_centers = bubbleCenters; nbubbles = numBubbles;
 data = table(occluded, bubble_centers, bubbleSigmas, nbubbles, ...
     black, imagesVisible);
-
 save(sprintf('%s/data.mat', targetDirectory), 'images', 'data');
-occludedFilenames = arrayfun(@num2str, wholeImageNumsToOcclude, 'UniformOutput', false);
-saveImages([targetDirectory, '/occluded/'], occludedImages, occludedFilenames);
-wholeFilenames = arrayfun(@num2str, wholeImageNumsToOcclude, 'UniformOutput', false);
-saveImages([targetDirectory, '/whole/'], images, wholeFilenames);
+
+fprintf('saving images...\n');
+wholeFilenames = arrayfun(@(i) relativeImagePaths{i}, wholeImageNumsToOcclude, 'UniformOutput', false);
+% TODO: the following is not going to work for non-1:1 ratios
+occludedFilenames = arrayfun(@(i) adjustFilenameOcclusion(relativeImagePaths{i}), wholeImageNumsToOcclude, 'UniformOutput', false);
+saveImages(targetDirectory, images, wholeFilenames);
+saveImages(targetDirectory, occludedImages, occludedFilenames);
+end
+
+function occlusionFilename = adjustFilenameOcclusion(filepath)
+[path, name, ext] = fileparts(filepath);
+occlusionFilename = [path, '/', name, '-occluded', ext];
 end
 
 function croppedImage = resizedCenterCrop(image, targetSize)
@@ -77,13 +113,14 @@ scale = max(targetSize ./ imageSize(1:2));
 resizedImage = imresize(image, scale);
 imageSize = size(resizedImage);
 
-cropXY = (imageSize(1:2) - targetSize) / 2;
+cropXY = round((imageSize(1:2) - targetSize) / 2);
 cropSize = targetSize;
-if cropXY(1) > cropXY(2)
+if cropXY(1) > 0
     cropSize = cropSize - [1, 0];
-else
-    cropSize = cropSize - [0, 1];
 end
+if cropXY(2) > 0
+    cropSize = cropSize - [0, 1];
+end % nothing to do for x=y=0
 croppedImage = imcrop(resizedImage, cat(2, fliplr(cropXY), fliplr(cropSize)));
 imageSize = size(croppedImage);
 assert(all(imageSize(1:2) == targetSize));
