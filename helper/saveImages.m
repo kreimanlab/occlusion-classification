57c9f32ec2e359f098393fe7850c3737cc688b05
Martin Schrimpf <m4rtinsch@gmail.com>
2018-01-26 13:26:10 -0500

    allow specification of filenames

--- helper/saveImages.m
+++ helper/saveImages.m
@@ -1,9 +1,20 @@
-function saveImages(dir, images)
+function saveImages(dir, images, filenames)
 %SAVEIMAGES saves the given images in the given directory
 if ~exist(dir, 'dir')
     mkdir(dir);
 end
+if ~exist('filenames', 'var')
+    filenames = arrayfun(@num2str, 1:numel(images), 'UniformOutput', false);
+end
 for i = 1:numel(images)
     image = images{i};
-    imwrite(image, [dir '/' num2str(i) '.jpg']);
+    targetPath = [dir '/' filenames{i}];
+    if ~ismember(targetPath, '.')
+         targetPath = [targetPath, '.jpg'];
+    end
+    targetDir = fileparts(targetPath);
+    if exist(targetDir, 'dir') ~= 7
+        mkdir(targetDir);
+    end
+    imwrite(image, targetPath);
 end
