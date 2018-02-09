0424722da7175715d4a06e07abfe5418daa1b79b
Martin Schrimpf <m4rtinsch@gmail.com>
2018-02-09 14:04:00 -0500

    add less occluded images

--- data/images/writeImagesFromMat.m
+++ data/images/writeImagesFromMat.m
@@ -2,11 +2,17 @@ function writeImagesFromMat(imageSize)
 directory = fileparts(mfilename('fullpath'));
 occludedDir = [directory, '/occluded'];
 mkdir(occludedDir);
+lessOccludedDir = [directory, '/lessOccluded'];
+mkdir(lessOccludedDir);
+
 images = load('data/KLAB325.mat');
 images = images.img_mat;
-data = load('data/data_occlusion_klab325v2.mat');
-data = data.data;
-bubbleSigmas = repmat(14, [size(data, 1), 10]);
+occlusionData = load('data/data_occlusion_klab325v2.mat');
+occlusionData = occlusionData.data;
+lessOcclusionData = load('data/lessOcclusion/data_occlusion_klab325-high_visibility.mat');
+lessOcclusionData = lessOcclusionData.data;
+
+bubbleSigmas = repmat(14, [size(occlusionData, 1), 10]);
 if ~exist('imageSize', 'var')
     imageSize = size(images{1}, 2);
 end
@@ -28,22 +34,36 @@ for c = 1:size(categories, 1)
     mkdir(categoryDir);
     occludedCategoryDir = [occludedDir, '/', category];
     mkdir(occludedCategoryDir);
+    lessOccludedCategoryDir = [lessOccludedDir, '/', category];
+    mkdir(lessOccludedCategoryDir);
     for i = indices
         %% whole
         baseImage = images{i};
         image = convertImage(baseImage, imageSize);
         imageBasename = sprintf('%03d', i);
-%         imwrite(image, [categoryDir, '/', imageBasename, '.png']);
+        imwrite(image, [categoryDir, '/', imageBasename, '.png']);
         %% occluded
-        dataSelection = find(data.pres == i)';
-        assert(unique(data.truth(dataSelection)) == c);
-        for row = dataSelection
-            occludedImage = occlude({baseImage}, data.nbubbles(row), ...
-                data.bubble_centers(row, :), bubbleSigmas(row, :));
+        occlusionDataSelection = find(occlusionData.pres == i)';
+        assert(unique(occlusionData.truth(occlusionDataSelection)) == c);
+        for row = occlusionDataSelection
+            occludedImage = occlude({baseImage}, occlusionData.nbubbles(row), ...
+                occlusionData.bubble_centers(row, :), bubbleSigmas(row, :));
             occludedImage = convertImage(occludedImage{1}, imageSize);
             filepath = [occludedCategoryDir, '/', imageBasename, ...
                 '-', sprintf('%d', row), '.png'];
-%             imwrite(occludedImage, filepath);
+            imwrite(occludedImage, filepath);
+        end
+        %% less-occluded
+        lessOcclusionDataSelection = find(lessOcclusionData.pres == i)';
+        assert(unique(lessOcclusionData.truth(lessOcclusionDataSelection)) == c);
+        for row = lessOcclusionDataSelection
+            lessOccludedImage = occlude({baseImage}, lessOcclusionData.nbubbles(row), ...
+                lessOcclusionData.bubble_centers(row, :), ...
+                lessOcclusionData.bubbleSigmas(row, :));
+            lessOccludedImage = convertImage(lessOccludedImage{1}, imageSize);
+            filepath = [lessOccludedCategoryDir, '/', imageBasename, ...
+                '-', sprintf('%d', row), '.png'];
+            imwrite(lessOccludedImage, filepath);
         end
     end
 end
