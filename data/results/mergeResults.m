50cca56c7a3a1f5d25bde9fe80009bad83533a51
Martin Schrimpf <m4rtinsch@gmail.com>
2018-02-09 14:04:39 -0500

    handle datasets

--- data/results/mergeResults.m
+++ data/results/mergeResults.m
@@ -10,8 +10,9 @@ for resIter = 1:numel(varargin)
         assert(numel(results) == numel(res));
         for i = 1:numel(results)
             r1 = results{i}; r2 = res{i};
-            results{i} = table2dataset(outerjoin(...
-                dataset2table(r1), dataset2table(r2), ...
+            if isa(r1, 'dataset') r1 = dataset2table(r1); end
+            if isa(r2, 'dataset') r2 = dataset2table(r2); end
+            results{i} = table2dataset(outerjoin(r1, r2, ...
                 'MergeKeys', true));
         end
     end
