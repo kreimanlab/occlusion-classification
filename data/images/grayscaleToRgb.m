c2866a5ad9be3666445dc02ee76292ceee4f69ec
Martin Schrimpf <m4rtinsch@gmail.com>
2018-02-09 14:04:19 -0500

    add clarifying comment about unnecessary permutation

--- data/images/grayscaleToRgb.m
+++ data/images/grayscaleToRgb.m
@@ -4,5 +4,6 @@ if strcmp(format, 'channels-first')
     image = permute(image, [3, 1, 2]); % reorder to channels first
 else
     assert(strcmp(format, 'channels-last'));
+    % no permutation necessary
 end
 end
